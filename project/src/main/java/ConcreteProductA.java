a1270ec7ebe43e4691629e3aaab49ee00ccc8c4b
mahdikheirkhah <mohammad.kheirkhah@gritlab.ax>
2025-11-07 22:29:17 +0200

    CircularLinkedList

--- project/src/main/java/ConcreteProductA.java
+++
@@ -1,7 +0,0 @@
-public class ConcreteProductA implements Product {
-    @Override
-    public void showDetails() {
-        System.out.println("This is ConcreteProductA.");
-    }
-
-}
--- project/src/main/java/ConcreteProductB.java
+++
@@ -1,6 +0,0 @@
-public class ConcreteProductB implements Product {
-    @Override
-   public void showDetails(){
-        System.out.println("This is ConcreteProductB.");
-    }
-}
